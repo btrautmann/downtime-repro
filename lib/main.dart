143990b0c12156b16249783014c377b3ac7c6572
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-08-21 12:40:51 -0400

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 const key = 'sample';
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
   const usePrefs = true; // Change this value during repro
   // Theory: Wait for data from preferences
   final child = usePrefs
       // ignore: dead_code
       ? MainApp(
-          sampleString: (prefs) => prefs!.getString(key),
-          prefs: await SharedPreferences.getInstance(),
-        )
+          // sampleString: (prefs) => prefs!.getString(key),
+          // prefs: await SharedPreferences.getInstance(),
+          )
       // ignore: dead_code
       : MainApp(
-          sampleString: (_) => 'NOT_FROM_PREFS',
-          prefs: null,
-        );
+          // sampleString: (_) => 'NOT_FROM_PREFS',
+          // prefs: null,
+          );
   runApp(child);
 }
 
 class MainApp extends StatelessWidget {
-  final Function(SharedPreferences?) sampleString;
-  final SharedPreferences? prefs;
+  // final Function(SharedPreferences?) sampleString;
+  // final SharedPreferences? prefs;
 
   const MainApp({
     super.key,
-    required this.sampleString,
-    required this.prefs,
+    // required this.sampleString,
+    // required this.prefs,
   });
 
   @override
@@ -38,16 +38,16 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(sampleString(prefs)),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Text('BOOP'),
               ),
-              FilledButton(
-                onPressed: () async {
-                  await prefs?.setString(key, 'Hello World');
-                },
-                child: const Text('Write'),
-              ),
+              // FilledButton(
+              //   onPressed: () async {
+              //     await prefs?.setString(key, 'Hello World');
+              //   },
+              //   child: const Text('Write'),
+              // ),
             ],
           ),
         ),

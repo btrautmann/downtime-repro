b3efc7d12388a64866b3f3646ef3a9fe972297c6
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-08-21 12:43:39 -0400

    remove pretty much everything

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 const key = 'sample';
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  const usePrefs = true; // Change this value during repro
-  // Theory: Wait for data from preferences
-  final child = usePrefs
-      // ignore: dead_code
-      ? MainApp(
-          // sampleString: (prefs) => prefs!.getString(key),
-          // prefs: await SharedPreferences.getInstance(),
-          )
-      // ignore: dead_code
-      : MainApp(
-          // sampleString: (_) => 'NOT_FROM_PREFS',
-          // prefs: null,
-          );
-  runApp(child);
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -32,24 +17,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 16),
-                child: Text('BOOP'),
-              ),
-              // FilledButton(
-              //   onPressed: () async {
-              //     await prefs?.setString(key, 'Hello World');
-              //   },
-              //   child: const Text('Write'),
-              // ),
-            ],
-          ),
+          child: Text('BOOP'),
         ),
       ),
     );
